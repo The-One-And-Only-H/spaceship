c41004e5de40ae68841cfe7aa1bc033ec62fcdb4
H <juana.galan@outlook.com>
2019-01-16 18:48:04 +0000

    Added score test block to end of JS script with new stylings in CSS

--- docs/stars.js
+++ docs/stars.js
@@ -94,3 +94,26 @@ function updateGameArea() {
     myGamePiece.newPos();
     myGamePiece.update();
 }
+
+// score
+class Score {
+  constructor() {
+    this.element = document.createElement("div");
+    this.element.classList.add("score");
+    document.body.appendChild(this.element);
+    this.counter = 0;
+    this.element.innerHTML = this.counter;
+  }
+  increaseScore(n) {
+    this.counter += n;
+    this.element.innerHTML = this.counter;
+  }
+}
+
+let score = new Score();
+
+setInterval(
+  function () {
+    score.increaseScore(1);
+  }, 2000
+)
